3adc370865f94975623085d8d58fece509778b3c
Misha <m.murasovs@gmail.com>
2020-03-17 17:43:19 +0100

    Can return item info

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const { log } = Apify.utils;
 
 Apify.main(async () => {
     const requestQueue = await Apify.openRequestQueue();
-    await requestQueue.addRequest({ url: 'https://www.amazon.com/Best-Sellers/zgbs/' });
+    await requestQueue.addRequest({ url: 'https://www.amazon.com/Best-Sellers/zgbs/amazon-devices/ref=zg_bs_nav_0' });
     const pseudoUrls = [new Apify.PseudoUrl('https://www.amazon.com/Best-Sellers[.*]')];
 
     const crawler = new Apify.PuppeteerCrawler({
@@ -27,8 +27,28 @@ Apify.main(async () => {
             },
         },
         handlePageFunction: async ({ request, page }) => {
+            // get category name
             const title = await page.title();
-            log.info(`The title of ${request.url} is: ${title}`);
+            log.info(`Processing... ${title}`);
+
+            // Info for the best selling items
+            // TODO: deal with items that are not found
+            const itemName = await page.$eval('div.p13n-sc-truncated', el => el.innerHTML);
+            const itemUrl = await page.$eval('a.a-link-normal', el => el.href);
+            const itemPrice = await page.$eval('span.p13n-sc-price', el => el.innerHTML);
+
+
+            const results = {
+                category: title,
+                items: {
+                    item: itemName,
+                    url: itemUrl,
+                    price: itemPrice,
+                },
+            };
+
+            console.log(results);
+
             await Apify.utils.enqueueLinks({
                 page,
                 selector: 'li > a',
@@ -36,7 +56,7 @@ Apify.main(async () => {
                 requestQueue,
             });
         },
-        maxRequestsPerCrawl: 10,
+        maxRequestsPerCrawl: 5,
         maxConcurrency: 10,
     });
 
