a15406aa1bbd02d0ed24b41f06532c6f9719c52b
Misha <m.murasovs@gmail.com>
2020-03-17 16:01:54 +0100

    Rewrite to use puppeteer with stealth

--- src/main.js
+++ src/main.js
@@ -1,56 +1,43 @@
 const Apify = require('apify');
-// const { URL } = require('url');
-const { log, enqueueLinks } = Apify.utils;
+
+const { log } = Apify.utils;
 
 Apify.main(async () => {
-    // Create a request queue instance and add a request
     const requestQueue = await Apify.openRequestQueue();
-    await requestQueue.addRequest({
-        url: 'https://www.amazon.com/Best-Sellers/zgbs',
-    });
+    await requestQueue.addRequest({ url: 'https://www.amazon.com/Best-Sellers/zgbs/' });
+    const pseudoUrls = [new Apify.PseudoUrl('https://www.amazon.com/Best-Sellers[.*]')];
 
-    const handlePageFunction = async ({ request, $ }) => {
-        const title = $('span[id^=productTitle] ').text();
-
-        const price = $('span[id^=priceblock_ourprice]')
-            .text()
-            .replace(/(\r\n|\n|\r)/gm, '');
-
-        const rating = `${$('span[data-hook^=rating-out-of-text]').text().trim()} stars`;
-
-        const availability = $('div[id^=availability] span')
-            .text()
-            .replace(/(\r\n|\n|\r)/gm, '')
-            .trim();
-
-        // Find out how to address inconsistencies before using this
-        // const description = $('ul li span').text();
-
-        const results = {
-            url: request.url,
-            title: title.trim(),
-            price: price.trim(),
-            available: availability,
-            rating: rating.trim(),
-        };
-
-        // not sure why this doesn't work when not a const - find out
-        const enqueued = await enqueueLinks({
-            $,
-            requestQueue,
-            selector: 'div.a-section a[href]',
-            pseudoUrls: ['http[s?]://www.amazon.com[.*]/dp/[.*]'],
-            baseUrl: request.loadedUrl,
-        });
-
-        await log.info(results);
-        await Apify.pushData(results);
-    };
-
-    const crawler = new Apify.CheerioCrawler({
-        maxRequestsPerCrawl: 10,
+    const crawler = new Apify.PuppeteerCrawler({
         requestQueue,
-        handlePageFunction,
+        launchPuppeteerOptions: {
+            headless: true,
+            stealth: true,
+            useChrome: true,
+            stealthOptions: {
+                addPlugins: false,
+                emulateWindowFrame: false,
+                emulateWebGL: false,
+                emulateConsoleDebug: false,
+                addLanguage: false,
+                hideWebDriver: true,
+                hackPermissions: false,
+                mockChrome: false,
+                mockChromeInIframe: false,
+                mockDeviceMemory: false,
+            },
+        },
+        handlePageFunction: async ({ request, page }) => {
+            const title = await page.title();
+            log.info(`The title of ${request.url} is: ${title}`);
+            await Apify.utils.enqueueLinks({
+                page,
+                selector: 'li > a',
+                pseudoUrls,
+                requestQueue,
+            });
+        },
+        maxRequestsPerCrawl: 10,
+        maxConcurrency: 10,
     });
 
     await crawler.run();
