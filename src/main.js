06201ffdbde7abaf59cb14951d7a35b2e9f39367
Misha <m.murasovs@gmail.com>
2020-03-18 13:50:32 +0100

    Scrapes all items from all categories

--- src/main.js
+++ src/main.js
@@ -5,13 +5,9 @@ const { log } = Apify.utils;
 Apify.main(async () => {
     const requestQueue = await Apify.openRequestQueue();
 
-    const baseUrl = [
-        'https://www.amazon.com/Best-Sellers/zgbs/',
-    ];
-
+    // Best Sellers home page where category links are
+    const baseUrl = ['https://www.amazon.com/Best-Sellers/zgbs/'];
     const requestList = await Apify.openRequestList('categories', baseUrl);
-
-    // const pseudoUrls = [new Apify.PseudoUrl('https://www.amazon.com/Best-Sellers[.*]')];
 
     const crawler = new Apify.PuppeteerCrawler({
         requestList,
@@ -34,30 +30,48 @@ Apify.main(async () => {
             },
         },
         handlePageFunction: async ({ request, page }) => {
-            console.log('Processing:', request.url);
+            log.info('Processing:', request.url);
 
             if (request.userData.detailPage) {
                 // get category name
                 const title = await page.title();
-                // Info for the best selling items
-                // TODO: deal with items that are not found
-                const itemName = await page.$$eval('div.p13n-sc-truncated', el => el.innerHTML);
-                const itemUrl = await page.$$eval('a.a-link-normal', el => el.href);
-                const itemPrice = await page.$$eval('span.p13n-sc-price', el => el.innerHTML);
+                // Info for the best selling itemsObj
+                // TODO: deal with itemsObj that are not found
+                // TODO: add images
+                const itemsObj = await page.$$eval('div.p13n-sc-truncated', prods => prods.map(prod => prod.innerHTML));
+                const pricesObj = await page.$$eval('span.p13n-sc-price', price => price.map(el => el.innerHTML));
+                const urlsObj = await page.$$eval('span.aok-inline-block > a.a-link-normal', link => link.map(url => url.href));
+
+                // Transform the scraped objects into arrays
+                const itemsArr = [];
+                const pricesArr = [];
+                const urlsArr = [];
+                for (const product of itemsObj) itemsArr.push(product);
+                for (const price of pricesObj) pricesArr.push(price);
+                for (const link of urlsObj) {
+                    if (!urlsArr.includes(link)) {
+                        urlsArr.push(link);
+                    }
+                }
 
                 const results = {
                     category: title,
                     categoryUrl: request.url,
-                    items: {
-                        item: itemName,
-                        url: itemUrl,
-                        price: itemPrice,
-                    },
+                    items: {},
                 };
+                // Add scraped items into the results array
+                for (let i = 0; i < itemsArr.length; i++) {
+                    results.items[i] = {
+                        name: itemsArr[i],
+                        price: pricesArr[i],
+                        url: urlsArr[i],
+                    };
+                }
+
                 await Apify.pushData(results);
-                console.log('RESULTS: ', results);
             }
 
+            // Enqueue category pages on the Best Sellers homepage
             if (!request.userData.detailPage) {
                 await Apify.utils.enqueueLinks({
                     page,
@@ -67,10 +81,11 @@ Apify.main(async () => {
                         req.userData.detailPage = true;
                         return req;
                     },
+                    pseudoUrls: ['http[s?]://www.amazon.com/Best-Sellers[.*]'],
                 });
             }
         },
-        maxRequestsPerCrawl: 10,
+        maxRequestsPerCrawl: 40,
         maxConcurrency: 10,
     });
 
--- src/notes.js
+++
@@ -1,6 +0,0 @@
-const urlsToProcess = [
-    'https://www.amazon.com/Best-Sellers/zgbs/amazon-devices/ref=zg_bs_nav_0',
-    'https://www.amazon.com/Best-Sellers-Amazon-Launchpad/zgbs/boost/ref=zg_bs_nav_0',
-    'https://www.amazon.com/Best-Sellers-Prime-Pantry/zgbs/pantry/ref=zg_bs_nav_0',
-    'https://www.amazon.com/Best-Sellers-Appliances/zgbs/appliances/ref=zg_bs_nav_0',
-];
