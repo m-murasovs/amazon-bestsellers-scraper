e4aa24345c9ea12727601b99542708d336954702
levent91 <zleventcucuk@hotmail.com>
2022-04-27 23:14:55 +0300

    apply cheerio

--- src/getItems.js
+++ src/getItems.js
@@ -1,10 +1,11 @@
 /* eslint-disable max-len */
 const Apify = require('apify');
+const { load } = require('cheerio');
 
 const { log } = Apify.utils;
 const { LABEL } = require('./consts');
 
-async function getItems(pageObj, pageData, resultsArr, label) {
+async function getItems(request, pageObj, pageData, resultsArr, label) {
     if (label === LABEL.NEW) {
         // need to wait after page 1
         await pageObj.waitForSelector('.zg-grid-general-faceout');
@@ -14,31 +15,35 @@ async function getItems(pageObj, pageData, resultsArr, label) {
             await pageObj.waitForTimeout(5000);
         }
 
-        const allItems = await pageObj.$$('.zg-grid-general-faceout');
+        const $ = load(await pageObj.content());
 
-        for (const [index, item] of allItems.entries()) {
+        $('.zg-grid-general-faceout').each((index, element) => {
             const obj = {
                 ...pageData,
                 ID: index,
             };
-            obj.name = await item.$eval('div > a.a-link-normal:nth-of-type(2) > span > div', el => el.innerHTML);
-            const priceExists = (await item.$('.a-color-price')) || null;
-            obj.price = priceExists ? await item.$eval('.a-color-price', el => el.innerText) : null;
-            obj.url = await item.$eval('div > a.a-link-normal:nth-of-type(1)', url => url.href);
+            obj.name = $(element).find('div > a.a-link-normal:nth-of-type(2) > span > div').html();
+            const priceExists = $(element).find('.a-color-price').text() || null;
+            obj.price = priceExists ? $(element).find('.a-color-price').text() : null;
+            const foundUrl = $(element).find('div > a.a-link-normal:nth-of-type(1)').attr('href');
+            obj.url = foundUrl ? `https://${new URL(request.url).host}${foundUrl}` : null;
             // p13n-sc-dynamic-image
-            // const thumbSelector = await item.$('div[class*="_p13n-sc-dynamic-image"]') || await item.$('div[class*="_p13n-zg-list-grid-desktop_maskStyle"] > img') || '';
-            obj.thumbnail = await item.$eval('img', url => url.src);
+            // const thumbSelector = await element.$('div[class*="_p13n-sc-dynamic-image"]') || await element.$('div[class*="_p13n-zg-list-grid-desktop_maskStyle"] > img') || '';
+            obj.thumbnail = $(element).find('img').attr('src');
             resultsArr.push(obj);
-        }
+        });
         return;
     }
-    const itemsObj = await pageObj.$$eval('div.p13n-sc-truncated', prods => prods.map(prod => prod.innerHTML));
 
-    const pricesObj = await pageObj.$$eval('span.p13n-sc-price', price => price.map(el => el.innerHTML));
+    const $ = load(await pageObj.content());
 
-    const urlsObj = await pageObj.$$eval('span.aok-inline-block > a.a-link-normal', link => link.map(url => url.href));
+    const itemsObj = $('div.p13n-sc-truncated').html();
 
-    const imgsObj = await pageObj.$$eval('a.a-link-normal > span > div.a-section > img', link => link.map(url => url.src));
+    const pricesObj = $('span.p13n-sc-price').html();
+
+    const urlsObj = $('span.aok-inline-block > a.a-link-normal').attr('href');
+
+    const imgsObj = $('a.a-link-normal > span > div.a-section > img').attr('src');
 
     // Scrape all items that match the selector
 
@@ -64,10 +69,10 @@ async function getItems(pageObj, pageData, resultsArr, label) {
     }
 }
 
-async function scrapeDetailsPage(pageObj, pageData, label) {
+async function scrapeDetailsPage(request, pageObj, pageData, label) {
     const resultsArr = [];
     // Scrape page 1
-    await getItems(pageObj, pageData, resultsArr, label);
+    await getItems(request, pageObj, pageData, resultsArr, label);
     // Go to page 2 and scrape
     let nextPage;
     try {
@@ -78,7 +83,7 @@ async function scrapeDetailsPage(pageObj, pageData, label) {
     if (nextPage) {
         await nextPage.click();
         await pageObj.waitForNavigation();
-        await getItems(pageObj, pageData, resultsArr, label);
+        await getItems(request, pageObj, pageData, resultsArr, label);
         await Apify.pushData(resultsArr);
         log.info(`Saving results from ${await pageObj.title()}`);
     }
